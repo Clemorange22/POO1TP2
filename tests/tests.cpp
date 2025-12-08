c49a382d3327b6310d67d77a8278868512b09ec7
Clément Boittin <clement.boittin@proton.me>
2025-12-08 13:51:36 +0100

    Refactor test output formatting to enhance readability by adding descriptive print statements for each test

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,10 +3,19 @@
 #include "testsTrajetSimple.h"
 #include <iostream>
 
+using namespace std;
+
+void printTestName(const char *c) {
+  cout << "##### Test de " << c << " #####" << endl;
+}
+
 int main() {
   int count = 0;
+  printTestName("TrajetSimple");
   count += testsTrajetSimple();
+  printTestName("TrajetCompose");
   count += testsTrajetCompose();
+  printTestName("Parcours");
   count += testsParcours();
 
   return count;
