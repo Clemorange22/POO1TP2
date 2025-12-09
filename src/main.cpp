848ea38a719c7bc34646b70a8ee8b6dc6e45bc4a
Clément Boittin <clement.boittin@proton.me>
2025-12-09 01:15:14 +0100

    Reorder include statements for consistency and clarity in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
-#include "TrajetSimple.h"
-#include "Interface.h"
 #include "Catalogue.h"
+#include "Interface.h"
+#include "TrajetSimple.h"
 
-int main() 
+int main()
 // Il suffit d'appeler Interface.mainloop() pour démarrer le programme
 {
   Catalogue catalogue;
   Interface interface(&catalogue);
   interface.mainloop();
-  //Trajet **t1;
-  //*t1 = new TrajetSimple;
-  //t1[0] = new TrajetSimple("Plouër sur Rance", "Narvik", "Char à voile");
-  // t1[0]->Afficher();
+
   return 0;
 }
